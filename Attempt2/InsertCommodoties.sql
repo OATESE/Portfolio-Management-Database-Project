458fdd9f51652640027f98b96481fdb223840b47
OATESE <76813687+OATESE@users.noreply.github.com>
2024-03-15 17:54:34 +0000

    Insert Commodoties

---
+++ Attempt2/InsertCommodoties.sql
@@ -0,0 +1,6 @@
+INSERT INTO [PRACTICE2].[Assets] (Asset_Type, Symbol, Asset_Name, Industry)
+VALUES 
+('Commodity', 'GC=F', 'Gold', 'Precious Metals'),
+('Commodity', 'CL=F', 'Crude Oil WTI', 'Energy'),
+('Commodity', 'NG=F', 'Natural Gas', 'Energy'),
+('Commodity', 'SI=F', 'Silver', 'Precious Metals');
