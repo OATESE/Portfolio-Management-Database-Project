9ca53f81f2ff61e41001245368b53668af3352b7
elliott oates <elliottoates@icloud.com>
2024-03-19 15:30:17 +0000

    Fixed Portfolio View

--- Attempt3/PortfolioPerformanceview.sql
+++ Attempt3/PortfolioPerformanceview.sql
@@ -2,11 +2,11 @@ CREATE OR ALTER VIEW [PRACTICE3].[PortfolioHistoricalPerformanceView] AS
 SELECT
     p.Portfolio_ID,
     pr.Date,
-    SUM(pos.Quantity * pr.Price_Close) AS PortfolioValue,
+    SUM(CASE WHEN pos.PurchaseDate <= pr.Date THEN pos.Quantity * pr.Price_Close ELSE 0 END) AS PortfolioValue,
     ISNULL(SUM(CASE WHEN pos.PurchaseDate <= pr.Date THEN pos.Quantity * pos.PurchasePrice END), 0) AS CapitalInvested,
     CASE 
         WHEN ISNULL(SUM(CASE WHEN pos.PurchaseDate <= pr.Date THEN pos.Quantity * pos.PurchasePrice END), 0) > 0 
-        THEN (SUM(pos.Quantity * pr.Price_Close) - ISNULL(SUM(CASE WHEN pos.PurchaseDate <= pr.Date THEN pos.Quantity * pos.PurchasePrice END), 0)) 
+        THEN (SUM(CASE WHEN pos.PurchaseDate <= pr.Date THEN pos.Quantity * pr.Price_Close ELSE 0 END) - ISNULL(SUM(CASE WHEN pos.PurchaseDate <= pr.Date THEN pos.Quantity * pos.PurchasePrice END), 0)) 
              / NULLIF(ISNULL(SUM(CASE WHEN pos.PurchaseDate <= pr.Date THEN pos.Quantity * pos.PurchasePrice END), 0), 0) 
         ELSE NULL 
     END AS ROI
