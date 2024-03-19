8efefd9324c7e4fe997c63024bcac5f0eb41d8ca
elliott oates <elliottoates@icloud.com>
2024-03-19 16:20:24 +0000

    AssetAllocationView

--- Attempt3/CreateViews.sql
+++ Attempt3/CreateViews.sql
@@ -1,26 +1,41 @@
-CREATE VIEW [PRACTICE3].[AssetAllocationsView] AS
-SELECT
-    pv.Portfolio_ID,
-    pv.Date,
-    pv.Asset_Type,
-    pv.AssetTypeValue,
-    pv.AssetTypeValue / ptv.TotalValue AS Proportion
-FROM
-    (SELECT
+CREATE OR ALTER VIEW [PRACTICE3].[AssetAllocationView2] AS
+WITH PortfolioAssetValues AS (
+    SELECT
         p.Portfolio_ID,
         pr.Date,
         a.Asset_Type,
-        SUM(pos.Quantity * pr.Price_Close) AS AssetTypeValue
-    FROM [PRACTICE3].[Portfolios] p
+        SUM(pos.Quantity * pr.Price_Close) AS Value
+    FROM
+        [PRACTICE3].[Portfolios] p
     JOIN [PRACTICE3].[Positions] pos ON p.Portfolio_ID = pos.Portfolio_ID
     JOIN [PRACTICE3].[Assets] a ON pos.Asset_Symbol = a.AssetSymbol
     JOIN [PRACTICE3].[Prices] pr ON pos.Asset_Symbol = pr.Asset_Symbol
-    GROUP BY p.Portfolio_ID, pr.Date, a.Asset_Type) AS pv
-JOIN
-    (SELECT
+    GROUP BY p.Portfolio_ID, pr.Date, a.Asset_Type
+),
+PortfolioTotalValues AS (
+    SELECT
         Portfolio_ID,
         Date,
-        SUM(Quantity * Price_Close) AS TotalValue
-    FROM [PRACTICE3].[Positions] pos
-    JOIN [PRACTICE3].[Prices] pr ON pos.Asset_Symbol = pr.Asset_Symbol
-    GROUP BY Portfolio_ID, Date) AS ptv ON pv.Portfolio_ID = ptv.Portfolio_ID AND pv.Date = ptv.Date;
+        SUM(Value) AS TotalValue
+    FROM PortfolioAssetValues
+    GROUP BY Portfolio_ID, Date
+)
+SELECT
+    p.Portfolio_ID,
+    p.Date,
+    ISNULL(p.[Equity] / t.TotalValue, 0) AS 'EquityProportion',
+    ISNULL(p.[Bond] / t.TotalValue, 0) AS 'BondProportion',
+    ISNULL(p.[Commodity] / t.TotalValue, 0) AS 'CommodityProportion',
+    ISNULL(p.[Currency] / t.TotalValue, 0) AS 'CurrencyProportion',
+    ISNULL(p.[ETF] / t.TotalValue, 0) AS 'ETFProportion',
+    ISNULL(p.[MutualFund] / t.TotalValue, 0) AS 'MutualFundProportion',
+    ISNULL(p.[Option] / t.TotalValue, 0) AS 'OptionProportion',
+    ISNULL(p.[Future] / t.TotalValue, 0) AS 'FutureProportion',
+    ISNULL(p.[Cryptocurrency] / t.TotalValue, 0) AS 'CryptocurrencyProportion'
+FROM
+    PortfolioAssetValues
+PIVOT (
+    SUM(Value)
+    FOR Asset_Type IN ([Equity], [Bond], [Commodity], [Currency], [ETF], [MutualFund], [Option], [Future], [Cryptocurrency])
+) AS p
+JOIN PortfolioTotalValues t ON p.Portfolio_ID = t.Portfolio_ID AND p.Date = t.Date
