6aeec272a58e8678ee141ea5f2f274e21e9a154c
OATESE <76813687+OATESE@users.noreply.github.com>
2024-03-21 14:47:58 +0000

    Getting rid of unused/ practice files

--- Attempt3/Queries/Null.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM [PRACTICE3].[Positions]
-WHERE PurchasePrice IS NULL;
-
---SELECT * FROM [PRACTICE3].[Positions]
---WHERE PurchasePrice IS NULL;
---Some Nulls as the purchase date not 
--- Attempt3/delete.sql
+++
@@ -1,3 +0,0 @@
--- Delete rows from table 'TableName'
-DELETE FROM [CW1].[Positions]
-GO
